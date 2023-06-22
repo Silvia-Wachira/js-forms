e1bd16b48705783e0c1c45bf7c9dfab1ceb23052
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-06-22 10:55:09 +0300

    create a submit input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => (
-    document.querySelector('form').addEventListener('submit', (e) =>{
+    document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault()
         handleTodo(e.target.new_todo.value)
     } )
 ))
 
 function handleTodo(todo){
-    console.log(todo)
+    let p = document.createElement('p')
+    p.textContent = todo
+    console.log(p)
 }
