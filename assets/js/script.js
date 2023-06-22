40d1091221adcdcd0381f484244bbaa8a01783be
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-06-22 11:34:59 +0300

    build a delete function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => (
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault()
-        handleTodo(e.target.new_todo.value)
+        buildTodo(e.target.new_todo.value)
     } )
 ))
 
-function handleTodo(todo){
+function buildTodo(todo){
     let p = document.createElement('p')
-    p.textContent = todo
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = ' x '
+    p.textContent = `${todo} `
+    p.appendChild(btn)
     console.log(p)
+    // document.querySelector('#btn').appendChild(btn)
+    document.querySelector('#todo').appendChild(p)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
 }
